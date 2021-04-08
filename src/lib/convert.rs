ff2b237277f6d4e6d8d16b420f9ff42be2cc8fe8
PhotonQuantum <self@lightquantum.me>
2021-04-08 14:40:43 +0800

    feat(lib): convert image to mat without using fs buffer

--- src/lib/convert.rs
+++ src/lib/convert.rs
@@ -1,19 +1,25 @@
-use image::DynamicImage;
-use opencv::core::Mat;
-use opencv::imgcodecs;
-use tempfile::tempdir;
+use image::{DynamicImage, GenericImageView};
+use opencv::core::{Mat, Vec3b};
 
 use crate::errors::Result;
 
-// a naive implementation to convert rust image to opencv mat.
-// use file as a buffer. may have a better implementation.
-pub fn img_to_mat(img: &DynamicImage) -> Result<Mat> {
-    let dir = tempdir()?;
-    let filename = dir.path().join("temp.bmp");
-
-    img.save(&filename)?;
-    let mat = imgcodecs::imread(filename.to_str().unwrap(), imgcodecs::IMREAD_COLOR)?;
-
-    dir.close()?;
-    Ok(mat)
+// Convert rust image to opencv mat.
+// TODO: parallelize this conversion
+pub fn img_to_mat(image: &DynamicImage) -> Result<Mat> {
+    let (height, width) = (image.height() as usize, image.width() as usize);
+    let bytes = image.clone().into_bgr8().into_raw();
+    let mut mat_bytes: Vec<Vec<Vec3b>> = Vec::with_capacity(height);
+    let mut ptr = bytes.as_ptr();
+    for _ in 0..height {
+        let mut row = Vec::with_capacity(width);
+        for _ in 0..width {
+            unsafe {
+                let point = Vec3b::from([*ptr, *ptr.add(1), *ptr.add(2)]);
+                row.push(point);
+                ptr = ptr.add(3);
+            }
+        }
+        mat_bytes.push(row);
+    }
+    Ok(Mat::from_slice_2d(&mat_bytes)?)
 }
